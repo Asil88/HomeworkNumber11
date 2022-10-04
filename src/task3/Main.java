0e16e862350100a4334df0c453942d88d8c07ec5
Anton Lisovski <lisovskijj.99@gmail.com>
2022-10-04 12:49:06 +0300

    Task4,5

---
+++ src/task3/Main.java
@@ -0,0 +1,31 @@
+package task3;
+/*
+public class Main {
+    public static void main(String[] args) {
+        Task task = new Task("aaaaaaa assdasd asdsdasdsa pppp qwepp 1234");
+        task.foundWord();
+    }
+}
+class Task{
+    private String text;
+    private String result;
+
+    public Task(String text) {
+        this.text = text;
+    }
+
+    public  void foundWord(){
+        Character character=null;
+        char[] words = text.toCharArray();
+        for (Character c:words) {
+            if (c.equals(character)) {
+                continue;
+            } else {
+            result.concat(c.toString());
+
+            }
+
+        }
+        }
+
+    }*/
